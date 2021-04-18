6fc36e095e0a14a7a9c2a568905e654636684deb
Gleydson Rodrigues <gleydsonsr@gmail.com>
2021-04-18 17:45:52 -0300

    Update RepositoryItem.tsx

--- src/components/RepositoryItem/RepositoryItem.tsx
+++ src/components/RepositoryItem/RepositoryItem.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback } from 'react';
+import { Linking } from 'react-native';
 
 import { RepositoryItemProps } from './types';
 import { Container, Title, Description } from './styles';
 import { Button } from '../Button';
-import { Linking } from 'react-native';
 
 export default function Repository({ repository }: RepositoryItemProps) {
   const goToRepository = useCallback(() => {
